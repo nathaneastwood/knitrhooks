4c07f55a9f160265af455de47d77123275ae6a2f
nathaneastwood <nathan.eastwood@icloud.com>
2018-10-09 12:35:25 +0100

    doc: Add documentation for max_height

--- R/hooks.R
+++ R/hooks.R
@@ -1,3 +1,26 @@
+#' Add a scrollbar to R output
+#'
+#' Determine a maximum height that R console output should be and if output is
+#' larger than that value, make it scrollable.
+#'
+#' To run this hook, call the \code{max_height()} function and then you can
+#' either set a global maximum height
+#'
+#' \code{
+#' knitr::opts_chunk$set(max_height = "300px")
+#' }
+#'
+#' or you can specify it on a chunk by chunk basis
+#'
+#' \preformatted{
+#' ```{r max_height = "300px"}
+#' print(mtcars)
+#' ```
+#' }
+#'
+#' @examples
+#' max_height()
+#'
 #' @export
 max_height <- function() {
   knitr::knit_hooks$set(output = function(x, options) {
