4ffda93c541a8a8d88c9eda9e5f46be202ddf0a9
neastwood <nathan.eastwood@eqdata.co.uk>
2018-10-09 13:01:26 +0100

    doc: Rename max_height to output_max_height to avoid clashes with ggplot2::max_height.
    Closes #2.

--- R/hooks.R
+++ R/hooks.R
@@ -3,26 +3,26 @@
 #' Determine a maximum height that R console output should be and if output is
 #' larger than that value, make it scrollable.
 #'
-#' To run this hook, call the \code{max_height()} function and then you can
-#' either set a global maximum height
+#' To run this hook, call the \code{output_max_height()} function and then you
+#' can either set a global maximum height
 #'
 #' \code{
-#' knitr::opts_chunk$set(max_height = "300px")
+#' knitr::opts_chunk$set(output_max_height = "300px")
 #' }
 #'
 #' or you can specify it on a chunk by chunk basis
 #'
 #' \preformatted{
-#' ```{r max_height = "300px"}
+#' ```{r output_max_height = "300px"}
 #' print(mtcars)
 #' ```
 #' }
 #'
 #' @examples
-#' max_height()
+#' output_max_height()
 #'
 #' @export
-max_height <- function() {
+output_max_height <- function() {
   knitr::knit_hooks$set(output = function(x, options) {
     if (!is.null(options$max_height)) {
       paste0(
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 knitrhooks
 ==========
 
-**knitrhooks** provides useful knitr hooks to extend the functionality of knitr, Rmarkdown and bookdown.
+**knitrhooks** provides useful knitr hooks to extend the functionality of **knitr**, **Rmarkdown** and **bookdown**.
 
 Example
 -------
@@ -10,14 +10,14 @@ Example
 The current functionality provided by this package is to set a maximum height for R output and add a scroll bar to any output larger than a given height.
 
 <pre>
-<code>```{r max_width_example}
+<code>```{r max_height_example}
 library(knitrhooks)
-max_height()
+output_max_height()
 ```</code>
 
 # Example document
 
-<code>```{r max_height = "300px"}
+<code>```{r output_max_height = "300px"}
 print(mtcars)
 ```</code>
 </pre>
